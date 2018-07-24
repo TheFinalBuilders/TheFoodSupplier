17f9142bb3ac30a2a57c8ac51754d36e3e43fe3d
kpx01 <masa10448@gmail.com>
2018-07-25 01:27:47 +0900

    ブーメランの軌道修正

--- TheFoodSupplier/Assets/Scripts/Scene/InGame/BulletObject.cs
+++ TheFoodSupplier/Assets/Scripts/Scene/InGame/BulletObject.cs
@@ -9,36 +9,41 @@ public class BulletObject : MonoBehaviour {
 	bool isCollect = false;
 	public CharacterType characterType = CharacterType.Normal;
 	public int shotSpeed = 1;
-	public Vector3 velocity = Vector3.zero;
+	public Vector3 direction = Vector3.zero;
 	private float lifeTime = 0f;
+	private Vector3 parentPosition = Vector3.zero;
 
 	// Use this for initialization
 	void Start () {
 		lifeTime = 0f;
+		parentPosition = this.transform.parent.position;
+		this.transform.LookAt(this.transform.position + this.direction);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		lifeTime += Time.deltaTime;
 		switch(this.characterType){
 			case CharacterType.Normal:
-				this.transform.position += this.velocity * shotSpeed * Time.deltaTime;
+				this.transform.position += this.direction * shotSpeed * Time.deltaTime;
 				break;
 			case CharacterType.Boomerang:
-				if(!isReturn && this.lifeTime >= Mathf.PI * 2){
+				if(lifeTime > 1f){
 					isReturn = true;
 				}
-				lifeTime = lifeTime + Time.deltaTime;
-				this.transform.position += new Vector3(Mathf.Cos(lifeTime) * this.velocity.x, 0, Mathf.Sin(lifeTime) * this.velocity.z).normalized * 0.5f * shotSpeed * Time.deltaTime;
+				Vector3 offsetPosition = new Vector3(Mathf.Sign(this.transform.position.x) * (Mathf.Abs(this.transform.position.x) + 0.1f), 0f, this.transform.position.z);
+				transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(this.transform.parent.position - offsetPosition), lifeTime * lifeTime * 0.01f);
+				this.transform.position += this.transform.forward * shotSpeed * Time.deltaTime;
 				break;
 			default:
-				this.transform.position += this.velocity * shotSpeed * Time.deltaTime;
+				this.transform.position += this.direction * shotSpeed * Time.deltaTime;
 				break;
 		}
 	}
 
-	public void Init(CharacterType characterType, Vector3 velocity){
+	public void Init(CharacterType characterType, Vector3 direction){
 		this.characterType = characterType;
-		this.velocity = velocity;
+		this.direction = direction.normalized;
 	}
 
 	void OnTriggerEnter(Collider collider){
@@ -63,10 +68,11 @@ public class BulletObject : MonoBehaviour {
 	}
 
 	private void Return(){
+		Debug.Log("Return");
 		this.isReturn = true;
 		Vector3 direction = (this.transform.parent.position - this.transform.position);
 		direction.y = 0;
-		this.velocity = direction.normalized;
-		this.lifeTime = 0;
+		this.direction = direction.normalized;
+		this.transform.rotation = new Quaternion(this.transform.rotation.x, this.transform.rotation.y, -this.transform.rotation.z, this.transform.rotation.w);
 	}
 }
