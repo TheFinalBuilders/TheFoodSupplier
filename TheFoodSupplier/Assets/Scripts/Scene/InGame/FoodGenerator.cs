287677442b5ea2e43c9c41c7e74c0543de0d4b4f
kpx01 <masa10448@gmail.com>
2018-08-11 21:07:28 +0900

    laneType実装

--- TheFoodSupplier/Assets/Scripts/Scene/InGame/FoodGenerator.cs
+++ TheFoodSupplier/Assets/Scripts/Scene/InGame/FoodGenerator.cs
@@ -9,6 +9,7 @@ public class FoodGenerator : MonoBehaviour {
 	public Vector3 generatePosiotion;
 	public Vector3 exitPosiotion;
 	public LaneType laneType;
+	private LaneModel laneModel;
 
 	private float timer;
 
@@ -16,21 +17,23 @@ public class FoodGenerator : MonoBehaviour {
 	void Start () {
         this.generatePosiotion = new Vector3(-10, this.transform.position.y + 1.26f, this.transform.position.z);
         this.exitPosiotion = new Vector3(+12, this.transform.position.y + 1.26f, this.transform.position.z);
+		this.laneModel = LaneModel.Instance.GetLane(this.laneType);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		timer += Time.deltaTime;
-		if(timer >= 1.0f){
-			if(Random.Range(0f,100f) < 30f){
+		this.timer += Time.deltaTime;
+		if(this.timer >= this.laneModel.generateSpeed){
+			if(Random.Range(0f,100f) < this.laneModel.probability){
 				this.Generate();
 			}
-			timer = 0f;
+			this.timer = 0f;
 		}
 	}
 
 	void Generate(){
 		GameObject food = Instantiate (foodObject, generatePosiotion, Quaternion.identity);
 		food.transform.parent = this.transform;
+		food.GetComponent<FoodObject>().Init(this.laneModel.speed);
 	}
 }
--- TheFoodSupplier/Assets/Scripts/Scene/InGame/LaneModel.cs
+++ TheFoodSupplier/Assets/Scripts/Scene/InGame/LaneModel.cs
@@ -13,23 +13,34 @@ public class LaneModel {
 	public uint ID { get; private set; }
 	public float speed { get; private set; }
 	public float generateSpeed { get; private set; }
+	public float probability { get; private set; }
+	private static LaneModel instance = new LaneModel();
 
-	public LaneModel(uint id, float speed, float generateSpeed){
+	public static LaneModel Instance {
+		get {
+			return instance;
+		}
+	}
+
+	LaneModel(){}
+
+	private LaneModel(uint id, float speed, float generateSpeed, float probability){
 		this.ID = id;
 		this.speed = speed;
 		this.generateSpeed = generateSpeed;
+		this.probability = probability;
 	}
 
 	public LaneModel GetLane(LaneType lanetype){
 		switch(lanetype){
 			case LaneType.Forward :
-				return new LaneModel(0, 1, 0.5f);
+				return new LaneModel(0, 2f, 0.5f, 60f);
 			case LaneType.Middle :
-				return new LaneModel(1, 1.5f, 1f);
+				return new LaneModel(1, 1.5f, 1f, 40f);
 			case LaneType.Back :
-				return new LaneModel(2, 2f, 2f);
+				return new LaneModel(2, 1f, 2f, 20f);
 			default:
-				return new LaneModel(0, 1, 0.5f);
+				return new LaneModel(0, 2f, 0.5f, 60f);
 		}
 	}
 }
